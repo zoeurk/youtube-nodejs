fcaa621449f2b72d91b8ff5cc1a284b2a15f0b12
zoeurk <zoeurk@gmail.com>
2023-08-05 18:55:04 +0200

    commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# youtube-nodejs
+outil pour télécharger des videos depuis youtube.  
+nodejs youtube-nodejs.js url  
+l'url peut être un lien vers une video OU une playlist.
---
+++ youtube-node.js
@@ -0,0 +1,82 @@
+const ytdl = require('ytdl-core');
+const fs = require('fs');
+const ytpl = require('ytpl');
+const util = require('util');
+var ProgressBar = require('progress');
+var argv1 = process.argv.slice(2);
+var global = 1;
+function attendre(){
+	if(global == 0){
+		setInterval(attendre()
+			//console.log(global);
+		,1000);
+	}
+	console.log(global);
+	global = 0;
+}
+function telecharger_video(url, array, actuel, max){
+	//var url_ = url.toString();
+       	url_ = url.replace(/\&.*$/,"");
+	var id = url_.replace(/^.*=/i,"");
+	//console.log(id);
+	try{
+		ytdl.getInfo(id).then(info => {
+			var title = info.videoDetails.title;
+			var video = title.concat('',".mp4");
+			var labarre = title.concat('' , ': [:bar] :percent :etas');
+			ytdl(url_, { filter: 'audioandvideo' })
+			 .on('response', function(res){
+				bar = new ProgressBar(labarre, {
+				complete : String.fromCharCode(0x2588),
+				total    : parseInt(res.headers['content-length'], 10)	
+		  	})
+		 })
+		 .on('data', function(data){
+			 bar.tick(data.length);
+		 })
+		 .on('finish', function(){
+			let msg = title.concat('', ': OK.');
+		 	console.log(msg);
+			if(max > 0 && actuel < max){
+		 		telecharger_video(array[actuel+1], array, actuel+1, max);
+			}
+		 })
+		 .pipe(fs.createWriteStream(video))
+		});
+	}catch{
+		console.log(url_);
+	}
+	return 0;
+}
+const main = async function(url){
+  let saveString;
+  video = new Array();
+  // Save playlist
+  const url_ = url.toString();
+  const id = url_.replace(/^.*=/i,"");
+  const search = await ytpl(id, { limit: 15 });
+  //return 0;
+  saveString = util.inspect(search, { depth: Infinity });
+  var string = saveString.replace(/:\n/g,':');
+  string = string.replace(/(\{|\{) /g, '$1\n');
+  string = string.replace(/([a-zA-Z0-9]*):[ \t]+(.*[\,\n,\},\]])/g,'"$1":$2');
+  string = string.replace(/^"/,"");
+  string = string.replace(/"$/,"");
+  string = string.replace(/'/g,'"');
+  var myjson = JSON.parse(string);
+  //var video;
+  for(let x in myjson.items){
+	 video[x] = myjson.items[x].url;
+       	//video = myjson.items[x].url;
+	//console.log("fichier:%s [id = %s, url = %s]", myjson.items[x].title, myjson.items[x].id, video);
+	//console.log(ret);
+ }
+ telecharger_video(video[0],video, 0, video.length);
+}
+let str = argv1.toString();
+if(str.includes("playlist") === true){
+	main(str);
+}else{
+	telecharger_video(str, 0, 0, -1);
+	console.log("OK");
+}
